b6e354836b2954002930bf7e12ab69bd11b433d4
Aditi Garg <aditigarg39659@gmail.com>
2026-07-02 21:28:26 +0530

    Add first program

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-    cout<<"===== calculator =====" <<endl;
-    cout<<"1. Addition" <<endl;
-    cout<<"2. Subtraction" <<endl;
-    cout<<"3. Multiplication" <<endl;
-    cout<<"4. Division" <<endl;
-    cout<<"5. Exit" <<endl;
-        return 0;
+    cout<<"Hello world";
+    return 0;
 }
