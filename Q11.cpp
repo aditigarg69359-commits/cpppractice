09c38a95cb296e8952640a7fd0f234f77efe73d6
Aditi Garg <aditigarg39659@gmail.com>
2026-07-05 20:26:35 +0530

    practice questions Q1 to Q12 on variable and datatypes

---
+++ Q11.cpp
@@ -0,0 +1,11 @@
+// SWAP WITHOUT THIRD VARIABLE
+#include <iostream>
+using namespace std;
+int main(){
+    int a ,b;
+    cin >> a >> b;
+    swap (a , b);
+    cout << "a:" << a << endl;
+    cout << "b:" << b << endl;  
+      return 0;
+}
---
+++ Q12.cpp
@@ -0,0 +1,10 @@
+// AVERAGE OF 3 NUMBERS
+#include <iostream>
+using namespace std;
+int main(){
+    int a , b ,c;
+    cin >> a >> b >> c;
+    float Average = (a + b + c) / 3;
+    cout << "Average:" << Average;
+    return 0;
+}
